bf01ff7a5aaeb0aff0a6a8b7f4d88222e603ee36
Lina Karabash <linakarabash19@gmail.com>
2026-03-30 23:33:37 +0300

    Додавання функцій розв'язування задач 9.1 - 9.3

--- lab08/prj/ModulesKarabash.h
+++ lab08/prj/ModulesKarabash.h
@@ -2,5 +2,8 @@
 #define MODULESKARABASH_H
 
 double s_calculation(double x, double y, double z);
+void bonus_znuzhka (double summa);
+double faren_v_cels (double faren);
+int processNumber(int N);
 
 #endif
--- lab08/prj/ModulesKarabash/main.cpp
+++ lab08/prj/ModulesKarabash/main.cpp
@@ -51,3 +51,80 @@ double s_calculation (double x, double y, double z)
     double S = 1 + sqrt_expr;
     return S;
 }
+
+void bonus_znuzhka (double summa)
+{
+    int bonus = 0;
+    double result, znuzhka;
+    if (summa>=100 && summa<200)
+    {
+        bonus=1;
+    }
+    else if (summa>=200 && summa<500)
+    {
+        bonus=5;
+    }
+    else if (summa>=500 && summa<1000)
+    {
+        bonus=10;
+    }
+    else if (summa>=1000 && summa<2500)
+    {
+        bonus=50;
+    }
+    else if (summa>=2500 && summa<=5000)
+    {
+        bonus=150;
+    }
+    else if (summa>5000)
+    {
+        bonus=300;
+    }
+    else if (summa<100)
+    {
+        bonus=0;
+    }
+    znuzhka=double(bonus*0.25);
+    result = summa - znuzhka;
+    cout<<"Кількість бонусів: "<<bonus<<endl;
+    cout<<"Сума з урахуванням знижки: "<<result<<endl;
+}
+
+double faren_v_cels (double faren)
+{
+    double cels = double((5.0/9.0)*(faren-32));
+    return cels;
+}
+
+int processNumber(int N)
+{
+    bool countOnes = (N & (1 << 4)) != 0;
+    int flag =0;
+    int count = 0;
+    if (N == 0)
+        {
+        cout <<"Кількість двійкових нулів:"<<endl;
+        return countOnes ? 0 : 1;
+        }
+    int temp = N;
+    while (temp > 0)
+        {
+            int bit = temp % 2;
+            if (countOnes)
+            {
+                count += (bit == 1) ? 1 : 0;
+                flag=1;
+            }
+            else
+            {
+                count += (bit == 0) ? 1 : 0;
+            }
+            temp /= 2;
+        }
+        if (flag ==1)
+            cout <<"Кількість двійкових одиниць:"<<endl;
+        else
+            cout <<"Кількість двійкових нулів:"<<endl;
+
+    return count;
+}
