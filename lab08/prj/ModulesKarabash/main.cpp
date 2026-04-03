354cf1d9e5464e47485952ed98601b5f43683c2d
Lina Karabash <linakarabash19@gmail.com>
2026-04-03 22:42:03 +0300

    Додавання функцій реалізації задач 10.1-10.3

--- lab08/prj/ModulesKarabash/main.cpp
+++ lab08/prj/ModulesKarabash/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+#include <bitset>
+#include <sstream>
+#include <cstring>
 
 using namespace std;
 
@@ -128,3 +132,125 @@ int processNumber(int N)
 
     return count;
 }
+
+int vowelsCountInFile(string outputFileName, string ukrWord)
+{
+    ofstream outputFile(outputFileName, ios::app);
+    string vowels[10] = { "а", "е", "є", "и", "і", "ї", "о", "у", "ю", "я" };
+    int vowelsCount = 0;
+    size_t foundVowel;
+
+    for (int i = 0; i < 10; i++) {
+        foundVowel = ukrWord.find(vowels[i]);
+        if (foundVowel != string::npos) {
+            vowelsCount++;
+        }
+    }
+    outputFile << "Кількість голосних літер з вхідного файлу: " << vowelsCount << endl << endl;
+    outputFile.close();
+    return vowelsCount;
+}
+
+bool findWordInPoem(string outputFileName, string ukrWord)
+{
+    ofstream outputFile(outputFileName, ios::app);
+    string poemVI = { "до щастя не пускає лінощів орава.\n"
+                      "у чім воно - ніхто не знає до пуття.\n"
+                      "навчитись радісно робити кожну справу\n"
+                      "найперше правило щасливого життя\n" };
+    size_t foundWord;
+
+    foundWord = poemVI.find(ukrWord);
+    if (foundWord != string::npos) {
+        outputFile << "Знайдено слово \"" << ukrWord << "\"" << endl << endl;
+        outputFile.close();
+        return true;
+    }
+    outputFile << "Cлово \"" << ukrWord << "\" - не знайдено" << endl << endl;
+    outputFile.close();
+    return false;
+}
+
+int consonantsCountInFile(string inputFileName, string ukrWord)
+{
+    ofstream inputTxtFile(inputFileName, ios::app);
+    string vowels[10] = { "а", "е", "є", "и", "і", "ї", "о", "у", "ю", "я" };
+    size_t foundConsonants;
+
+    for (int i = 0; i < 10; i++) {
+        foundConsonants = ukrWord.find(vowels[i]);
+        if (foundConsonants != string::npos) {
+            ukrWord.erase(foundConsonants, vowels[i].length());
+        }
+    }
+    inputTxtFile << "Кількість приголосних літер з вхідного файлу: " << ukrWord.length() / 2 << endl << endl;
+    inputTxtFile.close();
+    return ukrWord.length() / 2;
+}
+
+bool timestampInFile(string inputFileName)
+{
+    ofstream inputFile(inputFileName, ios::app);
+    time_t rawtime;
+    time(&rawtime);
+    inputFile << "Дата та час дозапису інформаціїї: " << ctime(&rawtime) << endl << endl;
+    inputFile.close();
+    return true;
+}
+
+double sCalculationResInFile(const string& outputFileName, float x, float y, float z) {
+
+    double result = s_calculation(x, y, z);
+
+
+    ofstream outputFile(outputFileName, ios::app);
+
+
+    if (!outputFile) {
+        cerr << "Ошибка открытия файла: " << outputFileName << endl;
+        return -1.0;
+    }
+
+
+    outputFile << "Результат выполнения s_calculation: " << result << endl << endl;
+    outputFile.close();
+
+    return result;
+}
+
+bool decimalToBinary(string outputFileName, unsigned int number)
+{
+    if (number <= 0) {
+        return false;
+    }
+    ofstream outputFile(outputFileName, ios::app);
+    int binaryCels[32];
+    int i;
+    unsigned int convertedNum = number;
+
+    for(i = 0; convertedNum > 0; i++) {
+        binaryCels[i] = convertedNum % 2;
+        convertedNum = convertedNum / 2;
+    }
+    outputFile << "Двійкове представлення числа " << number << " : ";
+    for(i = i - 1; i >= 0; i--) {
+        outputFile << binaryCels[i];
+    }
+    outputFile << endl << endl;
+    outputFile.close();
+    return true;
+}
+
+bool authorInfo(string outputFileName)
+{
+    ofstream outputFile(outputFileName, ios::out);
+	outputFile << "====================================================================" << endl
+               << " Виконавець:         Карабаш Ліна Олександрівна" << endl
+               << " Рік розробки:       2026" << endl
+               << " Місто/Країна:       Кропивницький/Україна" << endl
+               << " ВНЗ:                Центральноукраїнський Національний Технічний Університет" << endl
+               << "====================================================================" << endl << endl;
+    outputFile.close();
+    return true;
+}
+
