ab2a1a80974f4ebdb720c58669485ac5424968a9
Lina Karabash <linakarabash19@gmail.com>
2026-04-04 00:08:51 +0300

    Завершення звіту

--- lab08/prj/ModulesKarabash.h
+++ lab08/prj/ModulesKarabash.h
@@ -1,9 +1,18 @@
 #ifndef MODULESKARABASH_H
 #define MODULESKARABASH_H
 
+#include <string>
+
 double s_calculation(double x, double y, double z);
 void bonus_znuzhka (double summa);
 double faren_v_cels (double faren);
 int processNumber(int N);
+int vowelsCountInFile(std::string outputFileName, std::string ukrWord);
+bool findWordInPoem(std::string outputFileName, std::string ukrWord);
+int consonantsCountInFile(std::string inputFileName, std::string ukrWord);
+bool timestampInFile(std::string inputFileName);
+double sCalculationResInFile(const std::string& outputFileName, float x, float y, float z);
+bool decimalToBinary(std::string outputFileName, unsigned int number);
+bool authorInfo(std::string outputFileName);
 
 #endif
